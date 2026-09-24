cadd15d043b5106b77b0ad7e8dc7ee6515f55c1b
Charles Aina <73085118+charles01a1@users.noreply.github.com>
2026-09-23 21:37:23 -0700

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        cache = [None for i in range(len(nums))]
+
+        def dp(i):
+            if i >= len(nums):
+                return 0
+
+            if cache[i] != None:
+                return cache[i]
+            choose = nums[i] + dp(i+2)
+            not_choose = dp(i+1)
+
+            best = max(choose, not_choose)
+            cache[i] = best
+
+            return best
+
+
+    
+        return dp(0)
